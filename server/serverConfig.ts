9aa1427aa35d1f416926f4ab68de4ce13784ea81
Patrick Edson <pedson@users.noreply.github.com>
2018-09-25 14:44:39 -0400

    Simplify configuration.

--- server/serverConfig.ts
+++ server/serverConfig.ts
@@ -1,29 +1,19 @@
-const configurations = {
-    development: {
-        port: 9683,
-        graphQlEndpoint: "/graphql",
-        graphQlHostname: "localhost",
-        graphQlPort: 9681,
-        authUser: "mouselight",
-        authPassword: "auth_secret" // always override this, but in case env is not set, don't leave completely open
-    },
-    production: {
-        port: 9683,
-        graphQlEndpoint: "/graphql",
-        graphQlHostname: "search-api",
-        graphQlPort: 9681
-    }
+const configuration = {
+    port: 9683,
+    graphQlEndpoint: "/graphql",
+    graphQlHostname: "search-api",
+    graphQlPort: 9681,
+    authUser: "mouselight",
+    authPassword: "auth_secret" // always override this, but in case env is not set, don't leave completely open
 };
 
 function loadServerConfiguration() {
-    let env = process.env.NODE_ENV || "development";
-
-    let config = configurations[env] || configurations.development;
+    let config = Object.assign({}, configuration);
 
     config.port = process.env.SEARCH_CLIENT_PORT || config.port;
     config.graphQlHostname = process.env.SEARCH_API_HOST || config.graphQlHostname;
     config.graphQlPort = process.env.SEARCH_API_PORT || config.graphQlPort;
-    config.authUser = process.env.SEARCH_AUTH_USER|| config.authUser;
+    config.authUser = process.env.SEARCH_AUTH_USER || config.authUser;
     config.authPassword = process.env.SEARCH_AUTH_PASS || config.authPassword;
 
     return config;
