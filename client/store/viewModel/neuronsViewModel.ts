305765f5a9c3ed779bdd1237fcf51d11f4703226
Patrick Edson <pedson@users.noreply.github.com>
2019-10-01 11:10:23 -0400

    Remove dead code.

--- client/store/viewModel/neuronsViewModel.ts
+++
@@ -1,7 +0,0 @@
-import {computed, observable} from "mobx";
-
-import {NeuronViewModel} from "../../viewmodel/neuronViewModel";
-
-export class NeuronsViewModel {
-    @observable public SelectedNeuron: NeuronViewModel | null = null;
-}
